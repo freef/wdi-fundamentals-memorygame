70dc9ee05550b79ed976d2b30beaff19155bb109
Freef <mtfreeland@gmail.com>
2019-01-16 22:33:42 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,28 @@
-var cardOne = "king";
-var cardTwo = "king";
+
+
 var cardThree = 'queen';
 var cardFour = 'queen';
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay= [];
+var cardOne = cards[1];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne, cardTwo);
+
+if(cardsInPlay.length===2){
 
 console.log("User flipped " + cardTwo);
 console.log("User flipped " + cardOne);
+
+if(cardsInPlay[0]===cardsInPlay[1]){
+	console.log("you found a match!");
+}
+else{
+	alert("Sorry, try again!");
+}
+
+}
+else{
+	console.log("Select a card");
+}
+
